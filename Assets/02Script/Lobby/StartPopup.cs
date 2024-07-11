7011ac2a9bfcd0388dd4e879a7d284c751342277
ChanhuiBae <cksgml3604@naver.com>
2024-07-12 00:24:04 +0900

    start popup

---
+++ Assets/02Script/Lobby/StartPopup.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StartPopup : MonoBehaviour
+{
+    private Image weaponIcon;
+    private Button start;
+    private Button cancel;
+
+    private void Awake()
+    {
+        
+    }
+}
