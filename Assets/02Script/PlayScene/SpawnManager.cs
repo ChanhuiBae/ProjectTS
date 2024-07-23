a3f451e35d0f4df665f1e7f667aed984b15aeff6
Onsenuri <117154843+Onsenuri@users.noreply.github.com>
2024-07-23 19:26:35 +0900

    테이블 수정

--- Assets/02Script/PlayScene/SpawnManager.cs
+++ Assets/02Script/PlayScene/SpawnManager.cs
@@ -80,7 +80,7 @@ public class SpawnManager : MonoBehaviour
 
     private void Start()
     {
-        //StartCoroutine(SpawnLogic());
+        StartCoroutine(SpawnLogic());
         StartCoroutine(SpawnBoss());
     }
 
@@ -283,7 +283,7 @@ public class SpawnManager : MonoBehaviour
 
     private IEnumerator SpawnBoss()
     {
-        //yield return YieldInstructionCache.WaitForSeconds(300);
+        yield return YieldInstructionCache.WaitForSeconds(300);
         Spawn(7, 3000, CretureType.Swarm_Boss);
         yield return YieldInstructionCache.WaitForSeconds(300);
         yield return YieldInstructionCache.WaitForSeconds(6f);
